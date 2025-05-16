3f575173e1f0d715117e7c23afc6bcce0449791e
Ogirimah <ameerogirimah@outlook.com>
2025-05-16 13:31:57 +0100

    Add the files for the intro to unity and plane projects

---
+++ Plane Challenge/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(25, 0, 3);
+    // private Quaternion cameraYDirection = new Quaternion(0, 180, 0, 1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+        // transform.rotation = cameraYDirection;
+    }
+}
---
+++ prototype_1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Use "public" so the variable can be used in unity by making it accessible outside its current class
+    // Private variables
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Move the vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the vehicle based on horizontal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
