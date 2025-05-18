adfd86c84823a8f1159b6801d7de6447b0038fcf
Ogirimah <ameerogirimah@outlook.com>
2025-05-18 13:24:29 +0100

    Add the completed Dog fetch project files

---
+++ Prototype_2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private bool spawned = false;
+    private float spawnDelay;
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ResetDelay();
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && !spawned)
+        {
+            spawnDelay = 1f;
+            spawned = true;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+    private void ResetDelay()
+    {
+        if (spawned && spawnDelay > 0)
+        {
+            spawnDelay -= Time.deltaTime;
+        }
+        else if (spawnDelay < 0)
+        {
+            spawnDelay = 0;
+            spawned = false;
+        }
+    }
+}
---
+++ Prototype_2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Spawn first random ball using the start delay
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    void Update()
+    {
+
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall()
+    {
+        spawnInterval = Random.Range(3f, 5f);
+        // Generate random ball index and random spawn position
+        int randomBallIndex = Random.Range(0, ballPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[randomBallIndex], spawnPos, ballPrefabs[randomBallIndex].transform.rotation);
+
+        // Recursively trigger the spawning of the next random ball using the interval
+        Invoke(nameof(SpawnRandomBall), spawnInterval);
+    }
+
+}
