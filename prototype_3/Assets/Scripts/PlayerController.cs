4b45c408a6a2d526c397a2296cf371beb27da260
Ogirimah <ameerogirimah@outlook.com>
2025-05-20 07:00:49 +0100

    Edit the PlayerController script to make the player jump while avoiding spamming. Also add obstacle to the scene

--- prototype_3/Assets/Scripts/PlayerController.cs
+++ prototype_3/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    private bool isOnTheGround = true;
+    public float gravityModifier;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnTheGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnTheGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnTheGround = true;
     }
 }
